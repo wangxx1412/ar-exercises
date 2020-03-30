2415e35993fd34a4c7977510e66341be38490864
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-30 22:57:20 +0000

    Complete count stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,3 +9,5 @@ Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel:true, womens_a
 Store.create(name:"Richmond", annual_revenue: 1260000, womens_apparel:true)
 
 Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel:true)
+
+puts Store.count;
