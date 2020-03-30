d184be6fdcd87682e71fa5875db18929066532ba
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-30 23:45:33 +0000

    Complete 5.3

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,3 +10,6 @@ puts "----------"
 # Your code goes here ...
 @total_revenue = Store.sum("annual_revenue")
 puts @total_revenue
+
+@cool_stores = Store.where("annual_revenue > 1000000").count();
+puts @cool_stores
