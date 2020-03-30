1559f3c8679ced7342de1ea62d908f02038d1e6c
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-30 23:19:55 +0000

    Added 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,8 +6,8 @@ puts "----------"
 # Your code goes below here ...
 Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel:true, womens_apparel:true)
 
-Store.create(name:"Richmond", annual_revenue: 1260000, womens_apparel:true)
+Store.create(name:"Richmond", annual_revenue: 1260000, mens_apparel:false,womens_apparel:true)
 
-Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel:true)
+Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel:true,womens_apparel:false)
 
 puts Store.count
