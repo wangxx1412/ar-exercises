caa3578c87fc755c04e66ad75260f5f6008d0cb8
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-30 23:21:56 +0000

    :

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -12,3 +12,6 @@ Store.create(name:"Surrey", annual_revenue: 224000, mens_apparel:false, womens_a
 Store.create(name:"Whistler", annual_revenue: 1900000, mens_apparel:true, womens_apparel:false)
 
 Store.create(name:"Yaletown", annual_revenue: 430000, mens_apparel:true, womens_apparel:true)
+
+@men_stores = Store.where(mens_apparel:true)
+
