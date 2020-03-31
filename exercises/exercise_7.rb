5b1c3f1fa2507d3d08f9725e39bcbbcd9fc0df35
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-31 01:53:23 +0000

    Complete 7.4

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,8 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
 # Store.create(name:"Co", annual_revenue: 100000, mens_apparel:true, womens_apparel:true)
-
 # Store.create(name:"Coquilium", annual_revenue: -1, mens_apparel:true, womens_apparel:true)
-
 # Store.create(name:"Coquilium", annual_revenue: 100000)
+
+# Ask the user for a store name (store it in a variable)
+@store_name = gets.chomp
+@errors = Store.create(name:@store_name).errors.messages
+
+@errors.each {|error| puts "#{error[1][0]}"}
--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validates :name, length: { minimum: 3 }
-  validates :annual_revenue, numericality: { only_integer: true, greater_than:0 }
+  validates :name, length: { minimum: 3, message: "Name: Must be longer than 2 characters" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than:0, message:"Annual Revenue: Only interger larger than 0 is OK" }
   validate :store_must_carry_at_least_men_or_women_apparel
 
   def store_must_carry_at_least_men_or_women_apparel
     if (!womens_apparel) && (!mens_apparel)
-      errors.add(:name, "Store must carry at least men or women's apparel")
+      errors.add(:mens_apparel, "Apparel: Store must carry at least men or women's apparel")
     end
   end
 end
