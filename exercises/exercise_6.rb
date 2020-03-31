0e6d7338b37a07ec85867adcbe48a115d740b252
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-31 00:13:21 +0000

    Complete 6.4

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,9 +11,14 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find(1);
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Dorris", last_name: "Kim", hourly_rate: 20)
+@store1.employees.create(first_name: "Henry", last_name: "Olsen", hourly_rate: 60)
 
 @store2 = Store.find(2);
 @store2.employees.create(first_name: "Xiaoxuan", last_name: "Wang", hourly_rate: 40)
+@store2.employees.create(first_name: "Yvett", last_name: "Cevan", hourly_rate: 40)
+@store2.employees.create(first_name: "Todd", last_name: "Captain", hourly_rate: 40)
+@store2.employees.create(first_name: "Lily", last_name: "Rirr", hourly_rate: 40)
 
 @store4 = Store.find(4);
 @store4.employees.create(first_name: "Joe", last_name: "Doe", hourly_rate: 20)
