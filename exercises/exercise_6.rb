3da6297dffcb088448212b05d5869354224421a0
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-31 00:08:26 +0000

    Complete 6.3

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1);
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store2 = Store.find(2);
+@store2.employees.create(first_name: "Xiaoxuan", last_name: "Wang", hourly_rate: 40)
+
+@store4 = Store.find(4);
+@store4.employees.create(first_name: "Joe", last_name: "Doe", hourly_rate: 20)
+
+@store5 = Store.find(5);
+@store5.employees.create(first_name: "Donald", last_name: "Trump", hourly_rate: 30)
+
+@store6 = Store.find(6);
+@store6.employees.create(first_name: "Bob", last_name: "Lee", hourly_rate: 50)
